9df31720a5cbe1b11198cb19705d8e7b123648b0
General0917 <ht13a037@oecu.jp>
2023-07-14 04:30:20 +0900

    4-2の終わりまで

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -165,6 +165,77 @@
         </table>
     </form> --}}
 
+    {{-- <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form method="post" action="/Practice/Laravel/laravelapp/public/hello">
+        <table>
+            @csrf
+            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
+            <tr><th>age: </th><td><input type="number" name="age"></td></tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </table>
+    </form> --}}
+
+    {{-- <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
+    @endif
+    <form method="post" action="/Practice/Laravel/laravelapp/public/hello">
+        <table>
+            @csrf
+            @if ($errors->has('name'))
+            <tr><th>ERROR: </th><td>{{$errors->first('name')}}</td></tr>
+            @endif
+            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+
+            @if ($errors->has('mail'))
+            <tr><th>ERROR: </th><td>{{$errors->first('mail')}}</td></tr>
+            @endif
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+
+            @if ($errors->has('age'))
+            <tr><th>ERROR: </th><td>{{$errors->first('age')}}</td></tr>
+            @endif
+            <tr><th>age: </th><td><input type="number" name="age" value="{{old('age')}}"></td></tr>
+
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </table>
+    </form> --}}
+
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+        <p>入力に問題があります。再入力してください。</p>
+    @endif
+    <form method="post" action="/Practice/Laravel/laravelapp/public/hello">
+        <table>
+            @csrf
+            @error('name')
+                <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
+            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+
+            @error('mail')
+                <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+
+            @error('age')
+                <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
+            <tr><th>age: </th><td><input type="number" name="age" value="{{old('age')}}"></td></tr>
+
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </table>
+    </form>
 @endsection
 
 @section('footer')
