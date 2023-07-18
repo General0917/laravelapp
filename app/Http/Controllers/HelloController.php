4df5ed3dc2c302403cbab98b3debc5c8c70450c1
sakata-syo <sakata.syo@pocke.co.jp>
2023-07-18 21:00:38 +0900

    5-3の終わりまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -509,8 +509,201 @@ class HelloController extends Controller {
     //     return $response;
     // }
 
+    // public function index(Request $request) {
+    //     $items = DB::select('select * from people');
+    //     return view('hello.index', ['items' => $items]);
+    // }
+
+    // public function index(Request $request) {
+    //     if (isset($request->id)) {
+    //         $param = ['id' => $request->id];
+    //         $items = DB::select('select * from people where id = :id', $param);
+    //     } else {
+    //         $items = DB::select('select * from people');
+    //     }
+
+    //     return view('hello.index', ['items' => $items]);
+    // }
+
+    // public function index(Request $request) {
+    //     $items = DB::select('select * from people');
+    //     return view('hello.index', ['items' => $items]);
+    // }
+
+    // public function index(Request $request) {
+    //     $items = DB::table('people')->get();
+
+    //     return view('hello.index', ['items' => $items]);
+    // }
+
     public function index(Request $request) {
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
+
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function post(Request $request) {
         $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
+    }
+
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+
+    // public function create(Request $request) {
+    //     $param = [
+    //         'name' => $request->name,
+    //         'mail' => $request->mail,
+    //         'age' => $request->age,
+    //     ];
+
+    //     DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+
+    //     return redirect('/hello');
+    // }
+
+    public function create(Request $request) {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+
+        DB::table('people')->insert($param);
+
+        return redirect('/hello');
+    }
+
+    // public function edit(Request $request) {
+    //     $param = [
+    //         'id' => $request->id,
+    //     ];
+
+    //     $item = DB::select('select * from people where id = :id', $param);
+
+    //     return view('hello.edit', ['form' => $item[0]]);
+    // }
+
+    // public function update(Request $request) {
+    //     $param = [
+    //         'id' => $request->id,
+    //         'name' => $request->name,
+    //         'mail' => $request->mail,
+    //         'age' => $request->age
+    //     ];
+
+    //     DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+
+    //     return redirect('/hello');
+    // }
+
+    public function edit(Request $request) {
+        $item = DB::table('people')->where('id', $request->id)->first();
+
+        return view('hello.edit', ['form' => $item]);
+    }
+
+    public function update(Request $request) {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+
+        DB::table('people')
+             ->where('id', $request->id)
+             ->update($param);
+
+        return redirect('/hello');
+    }
+
+    // public function del(Request $request) {
+    //     $param = [
+    //         'id' => $request->id,
+    //     ];
+
+    //     $item = DB::select('select * from people where id = :id', $param);
+
+    //     return view('hello.del', ['form' => $item[0]]);
+    // }
+
+    // public function remove(Request $request) {
+    //     $param = [
+    //         'id' => $request->id,
+    //     ];
+
+    //     DB::delete('delete from people where id = :id', $param);
+
+    //     return redirect('/hello');
+    // }
+
+    public function del(Request $request) {
+        $item = DB::table('people')->where('id', $request->id)->first();
+
+        return view('hello.del', ['form' => $item]);
+    }
+
+    public function remove(Request $request) {
+        DB::table('people')
+             ->where('id', $request->id)->delete();
+
+        return redirect('/hello');
+    }
+
+    // public function show(Request $request) {
+    //     $id = $request->id;
+
+    //     $item = DB::table('people')->where('id', $id)->first();
+
+    //     // print_r($item);
+
+    //     return view('hello.show', ['item' => $item]);
+    // }
+
+    // public function show(Request $request) {
+    //     $id = $request->id;
+
+    //     $items = DB::table('people')->where('id', '<=', $id)->get();
+
+    //     // print_r($items);
+
+    //     return view('hello.show', ['items' => $items]);
+    // }
+
+    // public function show(Request $request) {
+    //     $name = $request->name;
+
+    //     $items = DB::table('people')
+    //          ->where('name', 'like', '%' . $name . '%')
+    //          ->orWhere('mail', 'like', '%' . $name . '%')
+    //          ->get();
+
+    //     // print_r($items);
+
+    //     return view('hello.show', ['items' => $items]);
+    // }
+
+    // public function show(Request $request) {
+    //     $min = $request->min;
+    //     $max = $request->max;
+
+    //     $items = DB::table('people')->whereRaw('age >= ? and age <= ?', [$min, $max])->get();
+
+    //     print_r($items);
+
+    //     return view('hello.show', ['items' => $items]);
+    // }
+
+    public function show(Request $request) {
+        $page = $request->page;
+        $items = DB::table('people')
+             ->offset($page * 3)
+             ->limit(3)
+             ->get();
+
+        print_r($items);
+
+        return view('hello.show', ['items' => $items]);
     }
 }
